a195dd2653c0e4cf654784b2f26a18311d06cebe
unknown <felipe.serfati@gmail.com>
2026-04-01 16:14:29 -0300

    subo readme file

---
+++ 12_MiPromedio/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class publicProgram
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("ingrese un monto total para que se reparta en 3 personas de forma igual");
+        Console.WriteLine("ingrese el monto ");
+        double monto = Convert.ToDouble(Console.ReadLine());
+        double equitativo = monto / 3 ;
+        Console.WriteLine("este es el monto:" + " " + equitativo);
+        
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ 9_CelciusFarenheit/9_CelciusFarenheit/9_CelciusFarenheit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_CelciusFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inserte una temperatura: ");
+            int radio = int.Parse(Console.ReadLine());
+            double Fahrenheit = (radio * 1.8) + 32;
+            double kelvin = radio + 273;
+            Console.WriteLine("valor de fahrenheit: " + Fahrenheit);
+            Console.WriteLine("valor de kelvin: " + kelvin);
+            Console.ReadKey();
+        }
+    }
+}
